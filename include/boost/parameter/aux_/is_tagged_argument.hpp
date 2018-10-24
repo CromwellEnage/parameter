5cfab56d0343467376d566652b52f797ce452d89
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2018-10-24 10:15:37 -0400

    Revert is_tagged_argument

--- include/boost/parameter/aux_/is_tagged_argument.hpp
+++ include/boost/parameter/aux_/is_tagged_argument.hpp
@@ -15,19 +15,11 @@ namespace boost { namespace parameter { namespace aux {
 
 #include <boost/mpl/bool.hpp>
 #include <boost/mpl/if.hpp>
-
-#if 1
-#include <boost/type_traits/is_base_of.hpp>
-#include <boost/type_traits/remove_const.hpp>
-#include <boost/type_traits/remove_reference.hpp>
-#else
 #include <boost/type_traits/is_convertible.hpp>
 #include <boost/type_traits/is_lvalue_reference.hpp>
-#endif
 
 namespace boost { namespace parameter { namespace aux {
 
-#if 0
     template <typename T>
     struct is_tagged_argument_aux
       : ::boost::is_convertible<
@@ -36,29 +28,15 @@ namespace boost { namespace parameter { namespace aux {
         >
     {
     };
-#endif
 
     // This metafunction identifies tagged_argument specializations
     // and their derived classes.
     template <typename T>
     struct is_tagged_argument
       : ::boost::mpl::if_<
-#if 1
-        // Cannot use is_convertible<> to check if T is derived from
-        // tagged_argument_base. -- Cromwell D. Enage
-            ::boost::is_base_of<
-                ::boost::parameter::aux::tagged_argument_base
-              , typename ::boost::remove_const<
-                    typename ::boost::remove_reference<T>::type
-                >::type
-            >
-          , ::boost::mpl::true_
-          , ::boost::mpl::false_
-#else
             ::boost::is_lvalue_reference<T>
           , ::boost::mpl::false_
           , ::boost::parameter::aux::is_tagged_argument_aux<T>
-#endif
         >::type
     {
     };
