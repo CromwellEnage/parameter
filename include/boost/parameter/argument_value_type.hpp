2d4dcfdfdc875ce1dacbfd6a8f2789045ba8eb1e
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2018-11-08 07:16:16 -0500

    Add boost/parameter/aux_/is_arg_list.hpp & boost/parameter/argument_value_type_fwd.hpp

--- include/boost/parameter/argument_value_type.hpp
+++ include/boost/parameter/argument_value_type.hpp
@@ -7,6 +7,15 @@
 #ifndef BOOST_PARAMETER_ARGUMENT_VALUE_TYPE_HPP
 #define BOOST_PARAMETER_ARGUMENT_VALUE_TYPE_HPP
 
+namespace boost { namespace parameter { namespace aux {
+
+    template <typename ArgList>
+    struct argument_list_value_type_impl
+    {
+        typedef typename ArgList::tagged_arg::value_type type;
+    };
+}}} // namespace boost::parameter::aux
+
 #include <boost/mpl/if.hpp>
 #include <boost/type_traits/is_same.hpp>
 
@@ -23,8 +32,25 @@ namespace boost { namespace parameter { namespace aux {
     };
 }}} // namespace boost::parameter::aux
 
-#include <boost/parameter/aux_/is_tagged_argument.hpp>
+#include <boost/parameter/argument_value_type_fwd.hpp>
 #include <boost/mpl/eval_if.hpp>
+
+namespace boost { namespace parameter { namespace aux {
+
+    template <typename ArgList, typename Keyword>
+    struct argument_list_value_type
+      : ::boost::mpl::eval_if<
+            ::boost::is_same<typename ArgList::tagged_arg::key_type,Keyword>
+          , ::boost::parameter::aux::argument_list_value_type_impl<ArgList>
+          , ::boost::parameter
+            ::argument_value_type<typename ArgList::tail_type,Keyword>
+        >
+    {
+    };
+}}} // namespace boost::parameter::aux
+
+#include <boost/parameter/aux_/is_tagged_argument.hpp>
+#include <boost/parameter/aux_/is_arg_list.hpp>
 #include <boost/mpl/identity.hpp>
 
 namespace boost { namespace parameter {
@@ -32,12 +58,15 @@ namespace boost { namespace parameter {
     template <typename T, typename Keyword>
     struct argument_value_type
       : ::boost::mpl::eval_if<
-            ::boost::parameter::aux_::is_tagged_argument<T>
+            ::boost::parameter::aux::is_tagged_argument<T>
           , ::boost::parameter::aux::argument_value_type<T,Keyword>
-          , ::boost::mpl::identity<void>
+          , ::boost::mpl::eval_if<
+                ::boost::parameter::aux::is_arg_list<T>
+              , ::boost::parameter::aux::argument_list_value_type<T,Keyword>
+              , ::boost::mpl::identity<void>
+            >
         >
     {
-        typedef void type;
     };
 }} // namespace boost::parameter
 
---
+++ include/boost/parameter/argument_value_type_fwd.hpp
@@ -0,0 +1,16 @@
+// Copyright Cromwell D. Enage 2018.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_PARAMETER_ARGUMENT_VALUE_TYPE_FWD_HPP
+#define BOOST_PARAMETER_ARGUMENT_VALUE_TYPE_FWD_HPP
+
+namespace boost { namespace parameter {
+
+    template <typename T, typename Keyword>
+    struct argument_value_type;
+}} // namespace boost::parameter
+
+#endif  // include guard
+
