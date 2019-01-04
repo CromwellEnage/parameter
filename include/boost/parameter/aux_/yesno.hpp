b1214018a7a0ca6983ea6b541cfeec0e65eadd9d
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2019-01-04 12:15:12 -0500

    Increase MP11 support

--- include/boost/parameter/aux_/yesno.hpp
+++ include/boost/parameter/aux_/yesno.hpp
@@ -17,10 +17,23 @@ namespace boost { namespace parameter { namespace aux {
 
     // mpl::true_ and mpl::false_ are not distinguishable by sizeof(),
     // so we pass them through these functions to get a type that is.
-    yes_tag to_yesno(::boost::mpl::true_);
-    no_tag to_yesno(::boost::mpl::false_);
-
+    ::boost::parameter::aux::yes_tag to_yesno(::boost::mpl::true_);
+    ::boost::parameter::aux::no_tag to_yesno(::boost::mpl::false_);
 }}} // namespace boost::parameter::aux
 
+#include <boost/parameter/config.hpp>
+
+#if defined(BOOST_PARAMETER_CAN_USE_MP11)
+#include <boost/mp11/integral.hpp>
+
+namespace boost { namespace parameter { namespace aux {
+
+    // mp11::mp_true and mp11::mp_false are not distinguishable by sizeof(),
+    // so we pass them through these functions to get a type that is.
+    ::boost::parameter::aux::yes_tag to_yesno(::boost::mp11::mp_true);
+    ::boost::parameter::aux::no_tag to_yesno(::boost::mp11::mp_false);
+}}} // namespace boost::parameter::aux
+
+#endif  // BOOST_PARAMETER_CAN_USE_MP11
 #endif  // include guard
 
