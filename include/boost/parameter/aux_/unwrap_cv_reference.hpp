ebec195beb64758836e57cb8f131a34f2981798e
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2019-01-05 00:20:23 -0500

    Disable MP11 usage for MSVC-14.0

--- include/boost/parameter/aux_/unwrap_cv_reference.hpp
+++ include/boost/parameter/aux_/unwrap_cv_reference.hpp
@@ -50,28 +50,22 @@ namespace boost { namespace parameter { namespace aux {
 
 #include <boost/tti/detail/dnullptr.hpp>
 
-#if defined(BOOST_PARAMETER_CAN_USE_MP11) && !( \
-        BOOST_WORKAROUND(BOOST_MSVC, >= 1910) && \
-        BOOST_WORKAROUND(BOOST_MSVC, < 1920) \
-    )
+#if defined(BOOST_PARAMETER_CAN_USE_MP11)
 #include <boost/mp11/integral.hpp>
 #include <boost/mp11/utility.hpp>
 #include <type_traits>
-#else   // !defined(BOOST_PARAMETER_CAN_USE_MP11) || MSVC-14.0
+#else   // !defined(BOOST_PARAMETER_CAN_USE_MP11)
 #include <boost/mpl/bool.hpp>
 #include <boost/type_traits/remove_reference.hpp>
 #if !BOOST_WORKAROUND(__BORLANDC__, BOOST_TESTED_AT(0x564)) && \
     !BOOST_WORKAROUND(BOOST_GCC, < 40000)
 #include <boost/mpl/eval_if.hpp>
 #endif
-#endif  // BOOST_PARAMETER_CAN_USE_MP11 && not MSVC-14.0
+#endif  // BOOST_PARAMETER_CAN_USE_MP11
 
 namespace boost { namespace parameter { namespace aux {
 
-#if defined(BOOST_PARAMETER_CAN_USE_MP11) && !( \
-        BOOST_WORKAROUND(BOOST_MSVC, >= 1910) && \
-        BOOST_WORKAROUND(BOOST_MSVC, < 1920) \
-    )
+#if defined(BOOST_PARAMETER_CAN_USE_MP11)
     // This metafunction returns mp11::mp_true if T is of type
     // reference_wrapper<U> cv.
     template <typename T>
@@ -100,7 +94,7 @@ namespace boost { namespace parameter { namespace aux {
       , ::std::remove_reference
       , T
     >;
-#else  // !defined(BOOST_PARAMETER_CAN_USE_MP11) || MSVC-14.0
+#else  // !defined(BOOST_PARAMETER_CAN_USE_MP11)
     // This metafunction returns mpl::true_ if T is of type
     // reference_wrapper<U> cv.
     template <typename T>
@@ -167,7 +161,7 @@ namespace boost { namespace parameter { namespace aux {
     {
     };
 #endif  // Borland or GCC 3- workarounds needed
-#endif  // BOOST_PARAMETER_CAN_USE_MP11 && not MSVC-14.0
+#endif  // BOOST_PARAMETER_CAN_USE_MP11
 }}} // namespace boost::parameter::aux
 
 #endif  // include guard
--- include/boost/parameter/config.hpp
+++ include/boost/parameter/config.hpp
@@ -50,7 +50,10 @@
     !defined(BOOST_NO_CXX11_STATIC_ASSERT) && \
     !defined(BOOST_NO_CXX11_HDR_TYPE_TRAITS) && \
     !defined(BOOST_NO_CXX11_HDR_INITIALIZER_LIST) && \
-    !defined(BOOST_NO_CXX11_HDR_TUPLE)
+    !defined(BOOST_NO_CXX11_HDR_TUPLE) && !( \
+        BOOST_WORKAROUND(BOOST_MSVC, >= 1910) && \
+        BOOST_WORKAROUND(BOOST_MSVC, < 1920) \
+    )
 // Boost.MP11 requires C++11. -- Cromwell D. Enage
 #define BOOST_PARAMETER_CAN_USE_MP11
 #endif
