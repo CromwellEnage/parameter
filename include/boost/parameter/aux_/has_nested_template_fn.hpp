760a431fb8abe38a20bf6c2035f66daf25c15d3b
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2019-01-09 00:42:35 -0500

    Reduce dependency on Boost.MPL

--- include/boost/parameter/aux_/has_nested_template_fn.hpp
+++ include/boost/parameter/aux_/has_nested_template_fn.hpp
@@ -6,18 +6,71 @@
 #ifndef BOOST_PARAMETER_AUX_HAS_NESTED_TEMPLATE_FN_HPP
 #define BOOST_PARAMETER_AUX_HAS_NESTED_TEMPLATE_FN_HPP
 
-#include <boost/mpl/has_xxx.hpp>
-
-namespace boost { namespace parameter { namespace aux {
-
-    BOOST_MPL_HAS_XXX_TEMPLATE_NAMED_DEF(has_nested_tpl_fn_impl, fn, false)
-}}} // namespace boost::parameter::aux
-
+#include <boost/parameter/aux_/yesno.hpp>
 #include <boost/parameter/config.hpp>
+#include <boost/tti/detail/dnullptr.hpp>
 
 #if defined(BOOST_PARAMETER_CAN_USE_MP11)
 #include <boost/mp11/integral.hpp>
 #include <boost/mp11/utility.hpp>
+#else
+#include <boost/mpl/bool.hpp>
+#include <boost/mpl/identity.hpp>
+#endif
+
+namespace boost { namespace parameter { namespace aux {
+
+    template <template <typename P0, typename P1> class F>
+    struct has_nested_template_fn_substitute_1
+    {
+    };
+
+    template <typename T>
+    class has_nested_template_fn_impl
+    {
+        template <typename U>
+        static BOOST_CONSTEXPR ::boost::parameter::aux::no_tag _check(...);
+
+        template <typename U>
+        static BOOST_CONSTEXPR ::boost::parameter::aux::yes_tag
+            _check(
+#if defined(BOOST_PARAMETER_CAN_USE_MP11)
+                ::boost::mp11::mp_identity<U> const volatile*
+#else
+                ::boost::mpl::identity<U> const volatile*
+#endif
+              , ::boost::parameter::aux::has_nested_template_fn_substitute_1<
+                    U::template fn
+                >* = BOOST_TTI_DETAIL_NULLPTR
+            );
+
+     public:
+#if defined(BOOST_PARAMETER_CAN_USE_MP11)
+        using type = ::boost::mp11::mp_bool<
+#else
+        typedef ::boost::mpl::bool_<
+#endif
+            sizeof(
+                ::boost::parameter::aux::has_nested_template_fn_impl<T>
+                ::template _check<T>(
+                    static_cast<
+#if defined(BOOST_PARAMETER_CAN_USE_MP11)
+                        ::boost::mp11::mp_identity<T> const volatile*
+#else
+                        ::boost::mpl::identity<T> const volatile*
+#endif
+                    >(BOOST_TTI_DETAIL_NULLPTR)
+                )
+            ) == sizeof(::boost::parameter::aux::yes_tag)
+#if defined(BOOST_PARAMETER_CAN_USE_MP11)
+        >;
+#else
+        > type;
+#endif
+    };
+}}} // namespace boost::parameter::aux
+
+#if defined(BOOST_PARAMETER_CAN_USE_MP11)
 #include <type_traits>
 #else
 #include <boost/type_traits/remove_const.hpp>
@@ -27,21 +80,17 @@ namespace boost { namespace parameter { namespace aux {
 
     template <typename T>
 #if defined(BOOST_PARAMETER_CAN_USE_MP11)
-    using has_nested_template_fn = ::boost::mp11::mp_if<
-        typename ::boost::parameter::aux
-        ::has_nested_tpl_fn_impl<typename ::std::remove_const<T>::type>::type
-      , ::boost::mp11::mp_true
-      , ::boost::mp11::mp_false
-    >;
+    using has_nested_template_fn = typename ::boost::parameter::aux
+    ::has_nested_template_fn_impl<typename ::std::remove_const<T>::type>
+    ::type;
 #else
     struct has_nested_template_fn
-      : ::boost::parameter::aux::has_nested_tpl_fn_impl<
-            typename ::std::remove_const<T>::type
+      : ::boost::parameter::aux::has_nested_template_fn_impl<
             typename ::boost::remove_const<T>::type
         >::type
     {
     };
-#endif  // BOOST_PARAMETER_CAN_USE_MP11
+#endif
 }}} // namespace boost::parameter::aux
 
 #endif  // include guard
